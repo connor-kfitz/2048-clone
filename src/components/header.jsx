93b3b38cfa622eb4542cd2e39162ea594bf05852
connor-kfitz <connorkfitzsimmons@gmail.com>
2023-04-01 15:14:28 -0400

    Responsive Design for Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,25 +2,29 @@ export default function Header( { currentScore, highScore, newGame } ) {
 
     return (
         <header className="header">
-            <div className="score-cont">
-                <span className="score-cont__title">2048</span>
-                <div className="score-cont__current-score">
-                    <span className="score-cont__score-title">SCORE</span>
-                    <span className="score-cont__score-text">{currentScore}</span>
-                </div>
-                <div className="score-cont__best-score">
-                    <span className="score-cont__score-title">BEST</span>
-                    <span className="score-cont__score-text">{highScore ? highScore : 0}</span>
-                </div>
-            </div>
+
             <div className="info-cont">
+                <span className="info-cont__title">2048</span>
                 <div className="info-cont__text">
                     Join the tiles, get to <span className="info-cont__text-bold">2048!</span>
                     <br/>
                     <a className="info-cont__rules-link" href="#rules">How to play →</a>
                 </div>
-                <button className="info-cont__new-game-button" onClick={newGame}>New Game</button>
             </div>
+
+            <div className="score-cont">
+                <div className="score-cont__current-score">
+                        <span className="score-cont__score-title">SCORE</span>
+                        <span className="score-cont__score-text">{currentScore}</span>
+                </div>
+                <div className="score-cont__best-score">
+                    <span className="score-cont__score-title">BEST</span>
+                    <span className="score-cont__score-text">{highScore ? highScore : 0}</span>
+                </div>
+                <button className="score-cont__new-game-button" onClick={newGame}>New Game</button>
+            </div>
+            
+
         </header>
     )
 }
--- src/components/row.jsx
+++ src/components/row.jsx
@@ -11,7 +11,9 @@ export default function Row({ row, rowIndex, translateData, translateDirection, 
                 translateDirection={translateDirection} key={key} mergeData={mergeData} newTileData={newTileData} endAnimation={endAnimation}/>
             ))}
 
-        {Array.from({ length: n }, (_, i) => <div className="game-background" style={{left: 10 + i*128}} key={i}></div>)}
+        {/* {Array.from({ length: n }, (_, i) => <div className="game-background" style={{left: 10 + i*128}} key={i}></div>)} */}
+
+        {Array.from({ length: n }, (_, i) => <div className="game-background" style={{left: 10 + i*78}} key={i}></div>)}
 
         </div>
     )
